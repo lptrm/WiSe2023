6ebf937f7449e2fffd58476ef37b1322bb993a88
jobernberg <jobernberg@stud.hs-heilbronn.de>
2023-11-02 19:41:10 +0100

    format

--- src/cryptographics/aes_problems/Problem_4_3.java
+++ src/cryptographics/aes_problems/Problem_4_3.java
@@ -3,13 +3,30 @@ package cryptographics.aes_problems;
 public class Problem_4_3 {
   int[][] multiplicationTable = new int[8][8];
   int irreduciblePolynomial = 0b000001011;
+
   public Problem_4_3() {
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
-          multiplicationTable[i][j] = multiply(i, j);
+        multiplicationTable[i][j] = multiply(i, j);
       }
     }
   }
+
+  public int multiply(int a, int b) {
+    int result = 0;
+    while (b != 0) {
+      if ((b & 1) != 0) {
+        result ^= a;
+      }
+      a <<= 1;
+      if ((a & 0b1000) != 0) {
+        a ^= irreduciblePolynomial;
+      }
+      b >>= 1;
+    }
+    return result;
+  }
+
   public static void printTablePolynomial(int[][] table) {
     for (int[] row : table) {
       for (int value : row) {
@@ -39,21 +56,8 @@ public class Problem_4_3 {
     }
     System.out.println();
   }
-public int multiply(int a, int b) {
-    int result = 0;
-    while (b != 0) {
-        if ((b & 1) != 0) {
-            result ^= a;
-        }
-        a <<= 1;
-        if ((a & 0b1000) != 0) {
-            a ^= irreduciblePolynomial;
-        }
-        b >>= 1;
-    }
-    return result;
-}
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     Problem_4_3 problem = new Problem_4_3();
     System.out.println("Multiplication Table:");
     Problem_4_2.printTable(problem.multiplicationTable);
